4ae9f828c6a661d8caa2d06c1956ac4bbaff1ccb
naveen <naveen@playo.co>
2023-10-19 11:31:49 +0530

    added chan

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     description='My private package from private github repo',
     url='https://github.com/naveeeen3000/booklib.git',
     license='unlicense',
-    packages=find_packages('test', include=['testmod']),
+    packages=['test'],
     zip_safe=False
 )
