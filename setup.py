d396a747f6a139f9bb7c91ffbcfedf59a384f168
naveen <naveen@playo.co>
2023-10-19 11:22:48 +0530

    added changes

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     description='My private package from private github repo',
     url='https://github.com/naveeeen3000/booklib.git',
     license='unlicense',
-    packages=find_packages(),
+    packages=find_packages('test', include=['testmod']),
     zip_safe=False
 )
