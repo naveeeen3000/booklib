d15a5491eb6b8ea9072d73d7820b64846f30dbd3
naveen <naveen@playo.co>
2023-10-19 10:42:22 +0530

    added some chagnes

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='test',
-    version='0.1',
-    description='Full featured redis cache backend for Django.',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='https://github.com/naveeeen3000/booklib.git',
+    license='unlicense',
+    packages=['test'],
+    zip_safe=False
 )
